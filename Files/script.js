1ccd9e5793486fb1746dfa6cd09b2d0d4a61cba7
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-28 17:02:38 -0300

    Adicionado reacao ao boss

--- Files/script.js
+++ Files/script.js
@@ -5,18 +5,29 @@ const playerHp = document.querySelector('#player-hp');
 const playerMp = document.querySelector('#player-mp');
 const bossHp = document.querySelector('#boss-hp');
 
+const bossAttack = () => {
+  playerHp.value -= 5;
+}
+
 pHit.addEventListener('click', () => {
   bossHp.value -= 5;
+  setTimeout(bossAttack, 500);
 });
 
 mHit.addEventListener('click', () => {
   bossHp.value -= 10;
   playerMp.value -= 5;
+  setTimeout(bossAttack, 500);
 });
 
 heal.addEventListener('click', () => {
-  if (bossHp.value < bossHp.max) {
-    bossHp.value += 5;
+  if (playerHp.value < playerHp.max && playerMp.value >= 5) {
     playerMp.value -= 5;
+    if ((playerHp.value + 6) >= playerHp.max) {
+      playerHp.value = playerHp.max;
+    } else {
+      playerHp.value += 6;
+    }
+    setTimeout(bossAttack, 500);
   }
 });
